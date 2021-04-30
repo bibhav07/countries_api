5f1185db0a10622c40d94a2007254c0887af4c4d
bibhav07 <59442413+bibhav07@users.noreply.github.com>
2021-04-30 17:58:08 +0530

    Add files via upload

---
+++ currencyapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Country_currency, Country_capital
+# Register your models here.
+
+admin.site.register(Country_currency)
+admin.site.register(Country_capital)
